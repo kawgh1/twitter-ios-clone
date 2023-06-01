f950d72243313a0fd3199f79b2341814da7bd281
K <krseven@gmail.com>
2023-05-31 19:58:42 -0500

    Initial Commit

---
+++ twitter-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-clone
+//
+//  Created by J on 5/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
