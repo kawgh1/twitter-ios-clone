98798032519156abd1f24b7ab9227d63f1327d89
kawgh1 <krseven@gmail.com>
2023-07-05 08:10:26 -0500

    worked on TweetHeader View

--- twitter-clone/View/TweetHeader.swift
+++ twitter-clone/View/TweetHeader.swift
@@ -40,6 +40,68 @@ class TweetHeader: UICollectionReusableView {
         return label
     }()
     
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize:18)
+        label.numberOfLines = 0
+        label.text = "Some test caption from spiderman for now"
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize:14)
+        label.textColor = .lightGray
+        label.textAlignment = .left
+        label.text = "6:33 PM - 1/28/2020"
+        return label
+    }()
+    
+    private lazy var optionsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .lightGray
+        button.setImage(UIImage(named: "down_arrow_24pt"), for: .normal)
+        button.addTarget(self, action: #selector(showActionSheet), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var retweetsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "2 Retweets"
+        return label
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0 Likes"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var statsView: UIView = {
+        let view = UIView()
+        
+        let divider1 = UIView()
+        divider1.backgroundColor = .systemGroupedBackground
+        view.addSubview(divider1)
+        divider1.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 8, height: 1.0)
+        
+        let stack = UIStackView(arrangedSubviews: [retweetsLabel, likesLabel])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.centerY(inView: view)
+        stack.anchor(left: view.leftAnchor, paddingLeft: 16)
+        
+        let divider2 = UIView()
+        divider2.backgroundColor = .systemGroupedBackground
+        view.addSubview(divider2)
+        divider2.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 1.0)
+        
+        return view
+    }()
     
     
     // MARK: - Lifecycle
@@ -49,7 +111,7 @@ class TweetHeader: UICollectionReusableView {
         
                 let labelStack = UIStackView(arrangedSubviews: [fullnameLabel, usernameLabel])
         labelStack.axis = .vertical
-        labelStack.spacing = 4
+        labelStack.spacing = -6
         
         let stack = UIStackView(arrangedSubviews: [profileImageView, labelStack])
         stack.axis = .horizontal // default
@@ -58,6 +120,18 @@ class TweetHeader: UICollectionReusableView {
         addSubview(stack)
         stack.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
         
+        addSubview(captionLabel)
+        captionLabel.anchor(top: stack.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16)
+
+        addSubview(dateLabel)
+        dateLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 20, paddingLeft: 16)
+        
+        addSubview(optionsButton)
+        optionsButton.centerY(inView: stack)
+        optionsButton.anchor(right:rightAnchor, paddingRight: 8)
+        
+        addSubview(statsView)
+        statsView.anchor(top: dateLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 20, height: 40)
     }
     
     required init?(coder: NSCoder) {
@@ -67,6 +141,12 @@ class TweetHeader: UICollectionReusableView {
     // MARK: - Selectors
     
     @objc func handleProfileImageTapped() {
-        
+        print("DEBUG: Go to User Profile..")
+
     }
+    
+    @objc func showActionSheet() {
+        print("DEBUG: Handle ShowActionSheet..")
+    }
+    
 }
